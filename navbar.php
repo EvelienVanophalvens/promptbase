cf504f6fdfb2756431c955bc4b39fb7944d944b4
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-04-10 09:08:18 +0200

    Feat: logout

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="css/style.css">
+<nav>
+    <div></div>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="logout.php">logout</a>
+        </li>
+    </ul>
+</nav>
