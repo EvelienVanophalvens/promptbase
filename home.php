599e41306ed11204833b53aa55e516ec7248d6ef
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-04-11 16:59:53 +0200

    feat: profile.php

--- home.php
+++ home.php
@@ -11,10 +11,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
-    <h2>dit is de home pagina na het inloggen</h2>
+    <div class="context">
+        <h2>Hello! Welcome to the home page.</h2>
+
+    </div>
     
 </body>
 </html>
---
+++ profile.php
@@ -0,0 +1,28 @@
+<?php
+    include_once(__DIR__."/bootstrap.php");    
+    include_once (__DIR__."/navbar.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn profiel</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+
+</head>
+<body>
+    <div class="context">
+        <div class="userinfo">
+            <div id="userProfilePicture">
+                <img src="https://placehold.co/200x200.png" alt="profile picture">
+            </div>
+            <h2 class="username">Louise</h2>
+            <p class="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat sed cras ornare arcu. Ut porttitor leo a diam sollicitudin tempor id. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Nulla pharetra diam sit amet. Magna eget est lorem ipsum dolor sit. In aliquam sem fringilla ut morbi tincidunt augue interdum. Odio morbi quis commodo odio aenean. Vitae auctor eu augue ut. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Facilisi cras fermentum odio eu feugiat. Ipsum dolor sit amet consectetur adipiscing elit ut. In arcu cursus euismod quis viverra nibh cras. Morbi tempus iaculis urna id. Ultrices tincidunt arcu non sodales.</p>
+        </div>
+        <div class="userPrompts">Hier komen de gemaakte prompts</div>
+    </div> 
+</body>
+</html>
