f2c6889710adba1e946763f671988b25ee500f3f
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-05-02 13:43:05 +0200

    feat: rapport user

--- accountView.php
+++ accountView.php
@@ -5,7 +5,6 @@ include_once(__DIR__."/navbar.php");
 authenticated();
 
 $user = User::getUser($_GET["user"]);
-
 $prompts = Prompts::getUserPrompts($user["id"]);
 
 $allExamples = [];
@@ -20,6 +19,18 @@ if(!empty($user["profilePicture"])) {
 } else {
     $profilePicturePath = "uploads/default_profile.png";
 }
+if(!empty($_POST["reason"]) && isset($_POST["report"])) {
+    try{
+    $report = new ReportsUser();
+    $report->setUserId($_GET["user"]);
+    $report->setReason($_POST["reason"]);
+    $report->save();
+    $message = "Your report has been sent";        
+    } catch(Throwable $e) {
+        $error = $e->getMessage();
+        var_dump($error);
+    }
+}
 
 ?>
 
@@ -30,28 +41,7 @@ if(!empty($user["profilePicture"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hello</title>
-    <script>
-    function toggleFollow() {
-        var button = document.getElementById("followButton");
-        
-        if(button.classList.contains("follow")) {
-            button.classList.remove("follow");
-            button.classList.add("unfollow");
-            button.innerHTML = "<a href='#'>Unfollow</a>";
-            button.style.backgroundColor = "#1F2937 ";
-            button.style.color = "white";
-        } else {
-            button.classList.remove("unfollow");
-            button.classList.add("follow");
-            button.innerHTML = "<a href='#'>Follow</a>";
-            button.style.backgroundColor = "#008CBA";
-            button.style.color = "white";
-        }
-
-    }
-
-
-</script>
+   
 
 </head>
 <body>
@@ -61,6 +51,16 @@ if(!empty($user["profilePicture"])) {
                 <img src="<?php echo htmlspecialchars($profilePicturePath)?>" alt="profile picture">
             </div>
             <h2 class="username"><?php echo htmlspecialchars($user['username'])?></h2>
+            <div id="dottedMenu">
+            <div class="hidden" id="promptMenu">
+                <p id="reporting">report prompt</p>
+            </div>
+            <div id="dots">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        </div>
                 <p id="bio-text"><?php echo htmlspecialchars($user["bio"]); ?></p>
                 <button id="followButton" class="submit small follow" onclick="toggleFollow()"><a href="#">Follow</a></button>
 
@@ -103,5 +103,50 @@ if(!empty($user["profilePicture"])) {
                     <?php endforeach;
                 }  ?>
             </div>
+            <form action="" method="POST" class= "middleForm">
+                <h2>You want to rapport this user</h2>
+                <label for="reason">reason</label>
+                <textarea name="reason" id="reason" placeholder="Type here your reason"></textarea>
+                <input type="hidden" name="report"  value="<?php echo $user["id"]?>">
+                <div class="form-element">
+                    <button type="submit" name="report" class="submit small">Send</button>
+                    <button type="submit" class="submit small" id="cancel">cancel</button>
+                </div>
+            </form>
 </body>
+
+<script>
+    function toggleFollow() {
+        var button = document.getElementById("followButton");
+        
+        if(button.classList.contains("follow")) {
+            button.classList.remove("follow");
+            button.classList.add("unfollow");
+            button.innerHTML = "<a href='#'>Unfollow</a>";
+            button.style.backgroundColor = "#1F2937 ";
+            button.style.color = "white";
+        } else {
+            button.classList.remove("unfollow");
+            button.classList.add("follow");
+            button.innerHTML = "<a href='#'>Follow</a>";
+            button.style.backgroundColor = "#008CBA";
+            button.style.color = "white";
+        }
+
+    }
+    document.querySelector('#dots').addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector('#promptMenu').classList.toggle('hidden');
+        console.log("e");
+    });
+    let report = false;
+    document.querySelector('#promptMenu #reporting').addEventListener('click', function(e){
+        e.preventDefault();
+        console.log("x");
+        document.querySelector('.profileContext').classList.add('faded');
+        document.querySelector(".middleForm").style.transform = "translate(-50%, -50%)";
+        report = true;
+    });
+</script>
+
 </html>
