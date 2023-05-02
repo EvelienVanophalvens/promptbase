67adaba5df87de68c788e3a94e7d451e927715b5
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-05-02 11:40:01 +0200

    fix: style credits in profile settings

--- profileSettings.php
+++ profileSettings.php
@@ -15,16 +15,18 @@ $user = User::getUser($_SESSION['userid']);
 
 </head>
 <body>
-    <div class="context">
-        <h2>Edit profile of <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>
-        <h3>Your password</h3>
-        <p>Attention: You are currently logged in as <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>. <br>When you change your password you will have to log in again with your new password.</p>
-        <button class="submit small"><a href="resetPassword.php">Edit password</a></button>
-        <h3>Delete your account</h3>
-        <p>Attention: You are currently logged in as <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>. <br>When you delete this account, you wont be able to log in again!</p>
-        <form action="deleteaccount.php" method="POST">
-            <button  name="user_delete" class="submit small danger" type="submit" value="<?= htmlspecialchars($_SESSION['auth_user']['username']);?>">Delete account</button>
-        </form>
+    <div class="content-settings">
+        <div class="actions">
+            <h2>Edit profile of <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>
+            <h3>Your password</h3>
+            <p>Attention: You are currently logged in as <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>. <br>When you change your password you will have to log in again with your new password.</p>
+            <button class="submit small"><a href="resetPassword.php">Edit password</a></button>
+            <h3>Delete your account</h3>
+            <p>Attention: You are currently logged in as <?php echo htmlspecialchars($_SESSION['auth_user']['username']); ?>. <br>When you delete this account, you wont be able to log in again!</p>
+            <form action="deleteaccount.php" method="POST">
+                <button  name="user_delete" class="submit small danger" type="submit" value="<?= htmlspecialchars($_SESSION['auth_user']['username']);?>">Delete account</button>
+            </form>
+        </div>
         <div class="credits">
             <h3>Credits</h3>
             <h3><?php echo htmlspecialchars($user['credits'])?></h3>
