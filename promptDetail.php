8dd9353d9c8fbf895f67374276ddfc1664cdaa2c
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-05-09 11:46:04 +0200

    Fix: Error in promptDetail

--- promptDetail.php
+++ promptDetail.php
@@ -54,7 +54,7 @@ if ($isFavorite) {
         <?php if(!empty($error)):?><p><?php echo $error ?></p><?php endif;?>
         <div class="container">
         <div class="title">
-            <h2><?php echo htmlspecialchars($prompt['prompts'][0]->promptName)?></h2>  
+            <h2><?php echo htmlspecialchars($prompt['prompts']->promptName)?></h2>  
             <p class="categoryLabel dark left">
             <?php if(isset($prompt["name"])) {
                 echo htmlspecialchars($prompt["prompts"]["name"]);
@@ -103,15 +103,15 @@ if ($isFavorite) {
         <section class="leftContainer" >
             <div class="promptUserInfo">
                 <div class="half">
-                    <a href="accountView.php?user=<?php echo htmlspecialchars($prompt['prompts'][0]->user)?>" ><p><strong>Made by </strong><?php echo htmlspecialchars($prompt['prompts'][0]->username)?> </p></a>
-                    <p><strong>Date:</strong> <?php echo htmlspecialchars($prompt["prompts"][0]->getdate())?></p>
+                    <a href="accountView.php?user=<?php echo htmlspecialchars($prompt['prompts']->user)?>" ><p><strong>Made by </strong><?php echo htmlspecialchars($prompt['prompts']->username)?> </p></a>
+                    <p><strong>Date:</strong> <?php echo htmlspecialchars($prompt["prompts"]->getdate())?></p>
                 </div>
             </div>
             <div class="promptPromptInfo">
                 <p class="title">
                     <h3>Description</h3>
                 </p>
-                <p class="half"><?php echo htmlspecialchars($prompt["prompts"][0]->getDescription());?></p>
+                <p class="half"><?php echo htmlspecialchars($prompt["prompts"]->getDescription());?></p>
              </div> 
             <div class="commentsection">
                 <p class="title">
@@ -142,8 +142,8 @@ if ($isFavorite) {
         <section class="rightContainer">
             <div class='likes dark'>
                 <svg class="whiteSvg" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>
-                <p data-id="<?php echo $prompt["prompts"][0]->getId(); ?>" class = "like">
-                    <span class='like' id = "counter" ><?php echo Prompts::getPromptLike($prompt["prompts"][0]->getId()); ?></span>
+                <p data-id="<?php echo $prompt["prompts"]->getId(); ?>" class = "like">
+                    <span class='like' id = "counter" ><?php echo Prompts::getPromptLike($prompt["prompts"]->getId()); ?></span>
                     Likes 
                 </p>
             </div>
@@ -154,10 +154,10 @@ if ($isFavorite) {
     </div>     
     
     <form action=""  method="POST" class= "middleForm">
-            <h2>You want to rapport prompt <?php  echo htmlspecialchars($prompt['prompts'][0]->promptName)?> </h2>
+            <h2>You want to rapport prompt <?php  echo htmlspecialchars($prompt['prompts']->promptName)?> </h2>
             <label for="reason">reason</label>
             <textarea name="reason" id="reason" placeholder="Type here your reason"></textarea>
-            <input type="hidden" name="report"  value="<?php echo $prompt["prompts"][0]->getId()?>">
+            <input type="hidden" name="report"  value="<?php echo $prompt["prompts"]->getId()?>">
             <div class="form-element">
                 <button type="submit" name="report" class="submit small">Send</button>
                 <button type="submit" class="submit small" id="cancel">cancel</button>
