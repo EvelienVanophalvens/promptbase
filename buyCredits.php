9bcb900a8e338ed2796976fcc0accd4a3b18205a
IlkeHeyvaert <86114403+IlkeHeyvaert@users.noreply.github.com>
2023-05-20 11:08:24 +0200

    fix: buy credits

--- buyCredits.php
+++ buyCredits.php
@@ -13,8 +13,9 @@ include_once(__DIR__."/navbar.php");
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
+  <div class="content">
 <a href="profileSettings.php" id="backbtn">< BACK TO OVERVIEW</a>
-
+</div>
     <div class="container2">
         <h1>Buy credits</h1>
         <form action="creditsPayment.php">
@@ -24,7 +25,8 @@ include_once(__DIR__."/navbar.php");
                 <div class="creditschoice">
   <img class="creditsImg" src="uploads/credits.png" alt="credits">
   <div class="credit">
-    <h2>25 credits</h2>              
+    <h2>25 credits</h2>
+    <h4>Prijs: €10</h4>              
     <a id="credit" href="creditsPayment.php?credits=25">Buy credits</a>
   </div>
 </div>
@@ -32,6 +34,7 @@ include_once(__DIR__."/navbar.php");
   <img class="creditsImg" src="uploads/credits.png" alt="credits">
   <div class="credit">
     <h2>50 credits</h2>
+    <h4>Prijs: €35</h4>
     <a id="credit" href="creditsPayment.php?credits=50">Buy credits</a>
   </div>
 </div>
@@ -39,6 +42,7 @@ include_once(__DIR__."/navbar.php");
   <img class="creditsImg" src="uploads/credits.png" alt="credits">
   <div class="credit">
     <h2>100 credits</h2> 
+    <h4>Prijs: €85</h4>
     <a id="credit" href="creditsPayment.php?credits=100">Buy credits</a>
   </div>
 </div>
